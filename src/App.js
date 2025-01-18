747b8fff3275198726e0629d5c2727fe98799bb1
Sahibxaae <mohamedsahib.xaae@gmail.com>
2025-01-18 22:24:20 +0530

    api integreted

--- src/App.js
+++ src/App.js
@@ -2,27 +2,27 @@ import { useState } from 'react';
 import './App.css';
 function App() {
   const [text,setText] = useState("");
-  const [size,setSize] = useState(200);
+  const [size,setSize] = useState();
+  const [image,setImage] = useState(false);
+  let url = `https://api.qrserver.com/v1/create-qr-code/?size=${size}x${size}&data=${text}`;
 
-  const qrGenerate = async () =>{
-    let url = `https://api.qrserver.com/v1/create-qr-code/?size=${size}x${size}&data=${text}`;
-
+  const handleText = (e) =>{
+    setText(e.target.value);
   }
-
-  const handleChange = () =>{
-    setText()
+  const handleSize = (e) => {
+    setSize(e.target.value);
   }
   return (
     <div className='app-container'>
       <h1>QR generator</h1>
       <div>
-  
+        {image && <img src={url}/>}   
       </div>
       <div className='text-box'>
-        <input type="text" className='text' placeholder='Enter url or text ...' onChange={handleChange} value={text} />
-        <input type="text" className='size' placeholder='Enter size Eg:200' value={size}/>
+        <input type="text" className='text' placeholder='Enter url or text ...' onChange={(e)=>handleText(e)} value={text} />
+        <input type="text" className='size' placeholder='Enter size Eg:200' value='' onChange={(e)=>handleSize(e)}/>
       </div>
-      <button>Generate QR</button>
+      <button onClick={()=> text!=="" && size!==0?setImage(!image):setImage(false)}>Generate QR</button>
     </div>
   );
 }
