1a873808e6f727484fe4255668177b95a833c20a
Sahibxaae <mohamedsahib.xaae@gmail.com>
2025-01-18 21:55:50 +0530

    api added for qr genrator

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
+import { useState } from 'react';
 import './App.css';
-
 function App() {
+  const [text,setText] = useState("");
+  const [size,setSize] = useState(200);
+
+  const qrGenerate = async () =>{
+    let url = `https://api.qrserver.com/v1/create-qr-code/?size=${size}x${size}&data=${text}`;
+
+  }
+
+  const handleChange = () =>{
+    setText()
+  }
   return (
-    <div>
+    <div className='app-container'>
       <h1>QR generator</h1>
+      <div>
+  
+      </div>
+      <div className='text-box'>
+        <input type="text" className='text' placeholder='Enter url or text ...' onChange={handleChange} value={text} />
+        <input type="text" className='size' placeholder='Enter size Eg:200' value={size}/>
+      </div>
+      <button>Generate QR</button>
     </div>
   );
 }
-
 export default App;
